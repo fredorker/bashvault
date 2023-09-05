b13f8b750d9493046e405dc22fec2b782aee7de9
fredorker <59013420+fredorker@users.noreply.github.com>
2023-09-05 13:11:17 +0200

    Update secrets2envfile.bash

--- secrets2envfile.bash
+++ secrets2envfile.bash
@@ -7,6 +7,6 @@ NL=$'\n'
 while read -r line; do
     key=$(echo $line | cut -d: -f1 | xargs)
     value=$(echo $line | cut -d: -f2 | xargs)
-    sed -i "/^${key}=/d" /shared/.env || true
+    sed -i "/^${key}=/d" $4 || true
     echo "${key}=${value}" >> $4
 done < <(vault kv get -format=yaml -field=data $3)
