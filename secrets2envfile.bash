f8ae1dd74c43985a4e293a7062f1bc9bb454aecb
fredorker <59013420+fredorker@users.noreply.github.com>
2023-09-14 16:19:37 +0200

    Update secrets2envfile.bash

--- secrets2envfile.bash
+++ secrets2envfile.bash
@@ -2,11 +2,12 @@
 
 export VAULT_TOKEN=$1
 export VAULT_ADDR=$2
+print "file ${3} and secret path ${4}"
 
 NL=$'\n'
 while read -r line; do
     key=$(echo $line | cut -d: -f1 | xargs)
     value=$(echo $line | cut -d: -f2 | xargs)
-    sed -i "/^${key}=/d" $4 || true
-    echo "${key}=${value}" >> $4
-done < <(vault kv get -format=yaml -field=data $3)
+    sed -i "/^${key}=/d" $3 || true
+    echo "${key}=${value}" >> $3
+done < <(vault kv get -format=yaml -field=data $4)
